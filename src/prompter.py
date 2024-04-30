96731fd8219442b8f3a00ce06c16d2a8f0b73f52
interstellarninja <ninjainterstellar@gmail.com>
2024-04-30 14:44:46 -0400

    adding system prompt formatting script

---
+++ src/prompter.py
@@ -0,0 +1,82 @@
+import datetime
+from pydantic import BaseModel
+from typing import Dict
+from src.schema import Agent
+from src.utils import (
+    get_fewshot_examples
+)
+import yaml
+import json
+import os
+
+class PromptSchema(BaseModel):
+    Role: str
+    Objective: str
+    Agents: str
+    Tools: str
+    Resources: str
+    Examples: str
+    Schema: str
+    Instructions: str 
+
+class PromptManager:
+    def __init__(self):
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        
+    def format_yaml_prompt(self, prompt_schema: PromptSchema, variables: Dict) -> str:
+        formatted_prompt = ""
+        for field, value in prompt_schema.dict().items():
+            if field == "Examples" and variables.get("examples") is None:
+                continue
+            formatted_value = value.format(**variables)
+            if field == "Instructions":
+                formatted_prompt += f"{formatted_value}"
+            else:
+                formatted_value = formatted_value.replace("\n", " ")
+                formatted_prompt += f"{formatted_value}"
+        return formatted_prompt
+
+    def read_yaml_file(self, file_path: str) -> PromptSchema:
+        with open(file_path, 'r') as file:
+            yaml_content = yaml.safe_load(file)
+        
+        prompt_schema = PromptSchema(
+            Role=yaml_content.get('Role', ''),
+            Objective=yaml_content.get('Objective', ''),
+            Agents=yaml_content.get('Agents', ''),
+            Tools=yaml_content.get('Tools', ''),
+            Resources=yaml_content.get('Resources', ''),
+            Examples=yaml_content.get('Examples', ''),
+            Schema=yaml_content.get('Schema', ''),
+            Instructions=yaml_content.get('Instructions', ''),
+        )
+        return prompt_schema
+    
+    def generate_prompt(self, user_prompt, tools, agents, resources, num_fewshot=None):
+        prompt_path = os.path.join(self.script_dir, '../configs', 'sys_prompt.yaml')
+        prompt_schema = self.read_yaml_file(prompt_path)
+
+        if num_fewshot is not None:
+            examples = get_fewshot_examples(num_fewshot)
+        else:
+            with open(os.path.join(self.script_dir, '../configs', 'example.txt'), 'r') as file:
+                examples = file.read()
+
+        schema_json = json.loads(Agent.schema_json())
+        #schema = schema_json.get("properties", {})
+
+        variables = {
+            "date": datetime.date.today(),
+            "agents": agents,
+            "tools": tools,
+            "resources": resources,
+            "examples": examples,
+            "schema": schema_json
+        }
+        sys_prompt = self.format_yaml_prompt(prompt_schema, variables)
+        print(sys_prompt)
+        prompt = [
+                {'content': sys_prompt, 'role': 'system'}
+            ]
+        prompt.extend(user_prompt)
+        return prompt
