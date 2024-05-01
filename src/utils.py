e06697653e532a6bfe0773dbe722a0ffc95e2061
interstellarninja <ninjainterstellar@gmail.com>
2024-05-01 14:08:34 -0400

    adding inference logger to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,35 @@
+import ast
+import datetime
+import importlib
 import inspect
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 import os
 import sys
+from typing import List
 
+logging.basicConfig(
+    format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d:%H:%M:%S",
+    level=logging.INFO,
+)
 script_dir = os.path.dirname(os.path.abspath(__file__))
+now = datetime.datetime.now()
+log_folder = os.path.join(script_dir, "inference_logs")
+os.makedirs(log_folder, exist_ok=True)
+log_file_path = os.path.join(
+    log_folder, f"function-calling-inference_{now.strftime('%Y-%m-%d_%H-%M-%S')}.log"
+)
+# Use RotatingFileHandler from the logging.handlers module
+file_handler = RotatingFileHandler(log_file_path, maxBytes=0, backupCount=0)
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s", datefmt="%Y-%m-%d:%H:%M:%S")
+file_handler.setFormatter(formatter)
+
+inference_logger = logging.getLogger("function-calling-inference")
+inference_logger.addHandler(file_handler)
 
 def get_tool_names():
     # Get all the classes defined in the script
