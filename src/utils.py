2d3cfbb892b0f22fc70ac0a4f619a5cfaa356913
interstellarninja <ninjainterstellar@gmail.com>
2024-05-06 09:54:57 -0400

    adding embedding functions to utils

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,12 @@ import re
 import sys
 from typing import List
 import xml.etree.ElementTree as ET
+import requests
+
+from langchain.text_splitter import CharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+from langchain_community.vectorstores import FAISS
+from unstructured.partition.html import partition_html
 
 
 logging.basicConfig(
@@ -119,3 +125,41 @@ def extract_json_from_markdown(text):
     else:
         print("JSON string not found in the text.")
     return None
+
+def embedding_search(url, ask):
+    text = download_form_html(url)
+    elements = partition_html(text=text)
+    content = "\n".join([str(el) for el in elements])
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=150, length_function=len, is_separator_regex=False,
+    )
+    docs = text_splitter.create_documents([content])
+    
+    # Load a pre-trained sentence transformer model
+    embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    
+    retriever = FAISS.from_documents(docs, embedding_model).as_retriever()
+    answers = retriever.get_relevant_documents(ask, top_k=4)
+    answers = "\n\n".join([a.page_content for a in answers])
+    return answers
+
+def download_form_html(url):
+    headers = {
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+      'Cache-Control': 'max-age=0',
+      'Dnt': '1',
+      'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+      'Sec-Ch-Ua-Mobile': '?0',
+      'Sec-Ch-Ua-Platform': '"macOS"',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
+      'Sec-Fetch-User': '?1',
+      'Upgrade-Insecure-Requests': '1',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.text
